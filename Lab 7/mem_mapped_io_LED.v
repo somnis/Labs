9368740cbe3794a61d4dd9dd880d823e472dd2fd
somnis <33208123+somnis@users.noreply.github.com>
2017-10-29 17:06:18 -0700

    Update mem_mapped_io_LED.v
    changed dff to reg with load enable

--- Lab 7/mem_mapped_io_LED.v
+++ Lab 7/mem_mapped_io_LED.v
@@ -1,5 +1,6 @@
-module mem_mapped_io_LED(write_data,mem_addr,mem_cmd,LED)
+module mem_mapped_io_LED(clk,write_data,mem_addr,mem_cmd,LED)
 
+    input clk;
     input [7:0] write_data;
     input [8:0] mem_addr;
     input [1:0] mem_cmd;
@@ -14,7 +15,6 @@ module mem_mapped_io_LED(write_data,mem_addr,mem_cmd,LED)
 
     assign load_LED = correct_addr & correct_cmd;
 
-//  vDFF(clk,D,Q)
+    register #(8) (clk, load_LED, write_data, LED);
 
-    vDFF #(8) (load_LED, write_data, LED);
-    
+endmodule 
